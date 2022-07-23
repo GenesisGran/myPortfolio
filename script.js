8a211d7918a181c78cf06bbf7c995c57fec28df8
GenesisGran <rafliichsanmahendra@gmail.com>
2022-07-23 12:02:11 +0700

    adding scroll animation

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ function showMore() {
     moreProjects.style.display = "flex";
   }
 }
+
+// reveal elements  on scroll
+window.addEventListener("scroll", reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (let i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 120;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+//
